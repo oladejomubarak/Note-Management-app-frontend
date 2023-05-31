785d22e83e48750b55c2629b70dd3c3c3ae53194
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-31 13:37:12 +0100

    calling registration api

--- src/Register.js
+++ src/Register.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import '../src/styles/Register.css';
 import LoginSideImage from '../src/assets/notePictures/images6.png';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 const Register = () => {
 const registerApi = "http://localhost:8082/api/v1/register";
+
+const data = {
+
+}
+
+const [ registerData, setRegisterData ] = useState()
+
+const handleChange = (e) => {
+  setRegisterData([...registerData,
+                  {[e.target.name] : e.target.value}
+  ])
+}
+
+const handleSubmit = (e) => {}
+  e.preventDefault();
+  Axios.post(registerApi, registerData)
+  .then(res => {
+    console.log(res)
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+
 
 
   return ( 
@@ -21,27 +47,32 @@ const registerApi = "http://localhost:8082/api/v1/register";
         <form >
           <label>First Name:</label>
           <input
+          namme="firstname"
           type="text"
           required
           />
           <label>Last Name:</label>
           <input
+          namme="lastname"
           type="text"
           required
           />
           <label>E-mail Address:</label>
           <input
+          namme="emailAddress"
           type="text"
           required
           />
           <label>Password:</label>
           <input
+          namme="password"
           type="text"
           required
           />
           <label>Confirm Password:</label>
           <input
           type="text"
+          namme="password"
           required
           />
           <button>Sign up</button>

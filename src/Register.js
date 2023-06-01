a9067c7f1bb5e60f5ec9ca11b25622c6190d7ecf
oladejomubarak <oladejomubarakade@gmail.com>
2023-06-01 12:52:50 +0100

    private lane chat edited

--- src/Register.js
+++ src/Register.js
@@ -1,91 +1,102 @@
 import { useState } from "react";
-import '../src/styles/Register.css';
-import LoginSideImage from '../src/assets/notePictures/images6.png';
-import { Link } from 'react-router-dom';
-import Axios from 'axios';
+import "../src/styles/Register.css";
+import LoginSideImage from "../src/assets/notePictures/images6.png";
+import { Link, useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 const Register = () => {
-const registerApi = "http://localhost:8082/api/v1/register";
+  const navigate = useNavigate();
+  const registerApi = "http://localhost:8082/api/v1/register";
 
-const data = {
+  const data = {
+    firstname: "",
+    lastname: "",
+    emailAddress: "",
+    password: "",
+    password: "",
+  };
 
-}
+  const [registerData, setRegisterData] = useState(data);
 
-const [ registerData, setRegisterData ] = useState()
+  const handleChange = (e) => {
+    setRegisterData({ ...registerData, [e.target.name]: e.target.value });
+  };
 
-const handleChange = (e) => {
-  setRegisterData([...registerData,
-                  {[e.target.name] : e.target.value}
-  ])
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicking is working");
+    console.log(registerData);
 
-const handleSubmit = (e) => {}
-  e.preventDefault();
-  Axios.post(registerApi, registerData)
-  .then(res => {
-    console.log(res)
-  }).catch(err => {
-    console.log(err);
-  })
-}
-
-
-
-
-  return ( 
+    Axios.post(registerApi, registerData)
+      .then((res) => {
+        console.log(res);
+        res.status === 200 && navigate("/verify-token");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
     <div className="register-page">
-    <div className="register-bg-image">
-      <img className='register-image' src={LoginSideImage} alt='register-image'/>
-
-    </div>
-    <div className="register-welcome-div">
-      <div className='welcome-div'>
-        <h1>NOTE MANAGEMENT</h1>
-        <h2>Create an account</h2>
+      <div className="register-bg-image">
+        <img
+          className="register-image"
+          src={LoginSideImage}
+          alt="register-image"
+        />
+      </div>
+      <div className="register-welcome-div">
+        <div className="welcome-div">
+          <h1>NOTE MANAGEMENT</h1>
+          <h2>Create an account</h2>
         </div>
-      <div className='register-form-div'>
-        <form >
-          <label>First Name:</label>
-          <input
-          namme="firstname"
-          type="text"
-          required
-          />
-          <label>Last Name:</label>
-          <input
-          namme="lastname"
-          type="text"
-          required
-          />
-          <label>E-mail Address:</label>
-          <input
-          namme="emailAddress"
-          type="text"
-          required
-          />
-          <label>Password:</label>
-          <input
-          namme="password"
-          type="text"
-          required
-          />
-          <label>Confirm Password:</label>
-          <input
-          type="text"
-          namme="password"
-          required
-          />
-          <button>Sign up</button>
+        <div className="register-form-div">
+          <form onSubmit={handleSubmit}>
+            <label>First Name:</label>
+            <input
+              name="firstname"
+              type="text"
+              onChange={handleChange}
+              required
+            />
+            <label>Last Name:</label>
+            <input
+              name="lastname"
+              type="text"
+              onChange={handleChange}
+              required
+            />
+            <label>E-mail Address:</label>
+            <input
+              name="emailAddress"
+              type="text"
+              onChange={handleChange}
+              required
+            />
+            <label>Password:</label>
+            <input
+              name="password"
+              type="password"
+              onChange={handleChange}
+              required
+            />
+            <label>Confirm Password:</label>
+            <input
+              name="confirmPassword"
+              type="password"
+              onChange={handleChange}
+              required
+            />
+            <button type="submit">Sign up</button>
 
-          <p>Already have an account? <Link to="/">Log in instead
-          </Link>
-          </p>
-        </form>
+            <p>
+              Already have an account? <Link to="/">Log in instead</Link>
+            </p>
+          </form>
+        </div>
       </div>
-      </div>
-  </div>
+    </div>
+  );
+};
 
-   );
-}
- 
 export default Register;

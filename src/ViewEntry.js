855f45ff7a597d353d5888c70ac7f35f20666eea
oladejomubarak <oladejomubarakade@gmail.com>
2023-07-06 10:30:02 +0100

    commiting.....

--- src/ViewEntry.js
+++ src/ViewEntry.js
@@ -35,7 +35,7 @@ const ViewEntry = () => {
       {entry ? (
         <article className='article'>
           <h1>{entry.title}</h1>
-          <div>{entry.body}</div>
+          <div className='body'>{entry.body}</div>
         <div className='dateAndTime'>
           <h2 className='dates'>Date: {entry.dateCreated}</h2>
           <h2>Time: {entry.timeCreated}</h2>
